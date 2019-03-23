d7ee322b15f56b64b00e06033858121667cab864
fatiharslann <fatih.46ars@gmail.com>
2019-03-23 11:32:24 +0300

    #15 Splash ekranı tasarımı yapıldı. Uygulamanın ana tasarımı çıkarılıp fragment yapısı kuruldu. Profil sayfası tasarlandı.

--- app/src/main/java/com/example/alisgram/MainActivity.java
+++ app/src/main/java/com/example/alisgram/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.alisgram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button txt = (Button) findViewById(R.id.button);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Profil.class);
+                startActivity(intent);
+            }
+        });
     }
 }
