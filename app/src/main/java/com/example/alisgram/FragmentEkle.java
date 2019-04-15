867b0edff35952a5123360897cf8deb5f4d2defd
Eren Ertaş <eren.ertas@hotmail.com>
2019-04-16 02:35:53 +0300

    Database eklemesi tamamlandı

--- app/src/main/java/com/example/alisgram/FragmentEkle.java
+++ app/src/main/java/com/example/alisgram/FragmentEkle.java
@@ -1,27 +1,53 @@
 package com.example.alisgram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-public class FragmentEkle extends Fragment implements TimePickerDialog.OnTimeSetListener {
-    MaterialSpinner Kategori_spinner, Alt_Kategori_spinner, Hatirlatici_spinner;
-    MaterialEditText Detay_edit, Etiket_edit;
-    Button Saat_Ekle;
-    TextView saat_goster;
+public class FragmentEkle extends Fragment {
+    private MaterialSpinner Kategori_spinner, Alt_Kategori_spinner;
+    private MaterialEditText Detay_edit, Etiket_edit;
+    private Button Saat_Ekle,btn_kaydet,pazartesi,sali,carsamba,persembe,cuma,cumartesi,pazar;
+    private TextView saat_text_1,saat_text_2,saat_text_3;
     private View view;
-    Switch hatirlatici_switch;
+    private Switch hatirlatici_switch,gizlilik_switch;
+    private ArrayList<String> list_gun,list_saat;
+    private EditText hatirlatici_text;
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private Intent maine_don;
+
+    private  String kategori_adi,alt_kategori_adi,etiket,detay,hatirlatici,tarih;
+    private boolean gizlilik;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +59,24 @@ public class FragmentEkle extends Fragment implements TimePickerDialog.OnTimeSet
     }
 
     private void tanimla() {
+        //--------------------------------------------------------------- Tarih
+
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        tarih = df.format(currentTime);
+
+        Log.d("TAGIM",tarih);
+        //-------------------------------------------------------------- User
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        //------------------------------------------ List
+
+        list_gun = new ArrayList<>();
+        list_saat = new ArrayList<>();
+
+        alt_kategori_adi = "Spor";
+        kategori_adi = "Yüzme";
 
         Alt_Kategori_spinner = (MaterialSpinner) view.findViewById(R.id.Alt_Kategori_spinner);
         Alt_Kategori_spinner.setItems("Yüzme", "Futbol", "Kitap Okuma", "Diş Fırçalama", "Diger");
@@ -40,7 +84,7 @@ public class FragmentEkle extends Fragment implements TimePickerDialog.OnTimeSet
 
             @Override
             public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
-                Toast.makeText(view.getContext(), item, Toast.LENGTH_SHORT).show();
+                alt_kategori_adi=item;
             }
         });
 
@@ -50,24 +94,20 @@ public class FragmentEkle extends Fragment implements TimePickerDialog.OnTimeSet
 
             @Override
             public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
-                Toast.makeText(view.getContext(), item, Toast.LENGTH_SHORT).show();
+                kategori_adi=item;
             }
         });
-        Hatirlatici_spinner = (MaterialSpinner) view.findViewById(R.id.Hatirlatici_spinner);
-        Hatirlatici_spinner.setItems("0", "1", "2", "3", "4");
-        Hatirlatici_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
-
-            @Override
-            public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
-                Toast.makeText(getActivity(), item, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        //--------------------------------------------------    Edit Text
         Detay_edit = view.findViewById(R.id.Detay_edit);
         Etiket_edit = view.findViewById(R.id.Etiket_edit);
+        hatirlatici_text = view.findViewById(R.id.hatirlatici_text);
+
+        //_--------------------------------------------------   Saat Kısmı
+        saat_text_1 = view.findViewById(R.id.saat_text_1);
+        saat_text_2= view.findViewById(R.id.saat_text_2);
+        saat_text_3 = view.findViewById(R.id.saat_text_3);
+
         Saat_Ekle = view.findViewById(R.id.Saat_Ekle);
-
-
         Saat_Ekle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,21 +116,197 @@ public class FragmentEkle extends Fragment implements TimePickerDialog.OnTimeSet
 
             }
         });
-
+        //------------------------------------------------- Switch
         hatirlatici_switch = view.findViewById(R.id.hatirlatici_switch);
         hatirlatici_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked == true) {
-                    Hatirlatici_spinner.setVisibility(View.VISIBLE);
+                    hatirlatici_text.setVisibility(View.VISIBLE);
                 } else {
-                    Hatirlatici_spinner.setVisibility(View.INVISIBLE);
+                    hatirlatici_text.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
+        gizlilik_switch = view.findViewById(R.id.gizliilik_switch);
+        gizlilik_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked == true) {
+                        gizlilik=true;
+                    } else {
+                        gizlilik=false;
+                    }
+                }
+
+        });
+        //-----------------------------------------------------------------------------------------         Günler
+        pazartesi = view.findViewById(R.id.Pazartesi_button);
+        sali = view.findViewById(R.id.Sali_Button);
+        carsamba = view.findViewById(R.id.Carsamba_button);
+        persembe = view.findViewById(R.id.Persembe_button);
+        cuma = view.findViewById(R.id.Cuma_button);
+        cumartesi = view.findViewById(R.id.Cumartesi_button);
+        pazar = view.findViewById(R.id.Pazar_button);
+
+
+        pazartesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pazartesi.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    pazartesi.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Pazartesi");
+                }else {
+                    pazartesi.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Pazartesi");
+                }
+            }
+        });
+        sali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sali.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    sali.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Salı");
+                }else {
+                    sali.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Salı");
+                }
+            }
+        });
+        carsamba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(carsamba.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    carsamba.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Çarşamba");
+                }else {
+                    carsamba.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Çarşamba");
+                }
+            }
+        });
+        persembe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(persembe.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    persembe.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Perşembe");
+                }else {
+                    persembe.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Perşembe");
+                }
+            }
+        });
+        cuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cuma.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    cuma.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Cuma");
+                }else {
+                    cuma.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Cuma");
+                }
+            }
+        });
+        cumartesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cumartesi.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    cumartesi.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Cumartesi");
+                }else {
+                    cumartesi.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Cumartesi");
+                }
+            }
+        });
+        pazar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pazar.getBackground().getConstantState()==getResources().getDrawable(R.drawable.circle).getConstantState()){
+                    pazar.setBackgroundResource(R.drawable.selected_circle);
+                    list_gun.add("Pazar");
+                }else {
+                    pazar.setBackgroundResource(R.drawable.circle);
+                    list_gun.remove("Pazar");
+                }
+            }
+        });
+        //  -   ------------------------------------------------------------------------------------    Kaydet
+        btn_kaydet = view.findViewById(R.id.kaydet_btn);
+        btn_kaydet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etiket = Etiket_edit.getText().toString();
+                detay = Detay_edit.getText().toString();
+                hatirlatici = hatirlatici_text.getText().toString();
+                if(!TextUtils.isEmpty(saat_text_1.getText().toString())){
+                    list_saat.add(saat_text_1.getText().toString());
+                }
+                if(!TextUtils.isEmpty(saat_text_2.getText().toString())){
+                    list_saat.add(saat_text_2.getText().toString());
+                }
+                if(!TextUtils.isEmpty(saat_text_3.getText().toString())){
+                    list_saat.add(saat_text_3.getText().toString());
+                }
+                if(!TextUtils.isEmpty(hatirlatici_text.getText().toString())){
+                    hatirlatici = hatirlatici_text.getText().toString();
+                }
+                getir();
+
+            }
+        });
+
+
     }
 
-    private void getir(){}
+    private void getir(){
+
+        if(!list_gun.isEmpty() && !list_saat.isEmpty()){
+
+            mDatabase = FirebaseDatabase.getInstance();
+            mDatabaseReference = mDatabase.getReference("aliskanliklar");
+            String key = mDatabaseReference.push().getKey();
+            ModelAliskanlik aliskanlik = new ModelAliskanlik();
+
+            aliskanlik.setAliskanlikId(key);
+            aliskanlik.setAliskanlikAltKategori(alt_kategori_adi);
+            aliskanlik.setAliskanlikDetay(detay);
+            aliskanlik.setAliskanlikDurum(0);
+            aliskanlik.setAliskanlikEtiket(etiket);
+            aliskanlik.setAliskanlikGizlilik(gizlilik);
+            aliskanlik.setAliskanlikGunler(list_gun);
+            aliskanlik.setAliskanlikKategori(kategori_adi);
+            aliskanlik.setAliskanlikKullaniciId(mCurrentUser.getUid());
+            aliskanlik.setAliskanlikOlusturulmaTarihi(tarih);
+            aliskanlik.setAliskanlikSaatler(list_saat);
+            aliskanlik.setAliskanlikSeviye(0);
+            aliskanlik.setAlislanlikOnHatirlatici(hatirlatici);
+
+            mDatabaseReference.child(key).setValue(aliskanlik);
+
+            list_saat.clear();
+            list_gun.clear();
+            pazartesi.setBackgroundResource(R.drawable.circle);
+            sali.setBackgroundResource(R.drawable.circle);
+            carsamba.setBackgroundResource(R.drawable.circle);
+            persembe.setBackgroundResource(R.drawable.circle);
+            cuma.setBackgroundResource(R.drawable.circle);
+            cumartesi.setBackgroundResource(R.drawable.circle);
+            pazar.setBackgroundResource(R.drawable.circle);
+
+        }
+        else{
+            Toast.makeText(getContext(), "Gün ve saat boş bırakılamaz", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+    }
+
 
 }
