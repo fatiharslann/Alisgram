2143a7fc9efce366fbddb32a79bb6d6909bd74ba
fatiharslann <fatih.46ars@gmail.com>
2019-04-16 12:27:57 +0300

    #26 Profil sayfası kullanıcıya ait tüm ve bitmiş alışkanlıklar çekildi.

--- app/src/main/java/com/example/alisgram/AliskanlikAdapter.java
+++ app/src/main/java/com/example/alisgram/AliskanlikAdapter.java
@@ -7,17 +7,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 
 public class AliskanlikAdapter extends RecyclerView.Adapter<AliskanlikAdapter.MyViewHolder> {
 
-    ArrayList<ModelAliskanlik> mAliskanlikList;
-    LayoutInflater inflater;
+    private ArrayList<ModelAliskanlik> mAliskanlikList;
+    private LayoutInflater inflater;
+    private DatabaseReference mDatabase;
 
     public AliskanlikAdapter(Context context, ArrayList<ModelAliskanlik> aliskanliklar) {
         inflater = LayoutInflater.from(context);
         this.mAliskanlikList = aliskanliklar;
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("aliskanliklar");;
     }
 
 
@@ -46,7 +51,7 @@ public class AliskanlikAdapter extends RecyclerView.Adapter<AliskanlikAdapter.My
         TextView aliskanlikEtiket, aliskanlikAciklama;
         ImageView deleteAliskanlik,editAliskanlik,gizlilikAliskanlik;
 
-        public MyViewHolder(View itemView) {
+        public MyViewHolder(final View itemView) {
             super(itemView);
             aliskanlikEtiket = (TextView) itemView.findViewById(R.id.profilAliskanlikAdi);
             aliskanlikAciklama = (TextView) itemView.findViewById(R.id.profilAliskanlikAciklama);
@@ -54,6 +59,13 @@ public class AliskanlikAdapter extends RecyclerView.Adapter<AliskanlikAdapter.My
             editAliskanlik = (ImageView) itemView.findViewById(R.id.editAliskanlik);
             gizlilikAliskanlik = (ImageView) itemView.findViewById(R.id.gizlilikAliskanlik);
 
+            /*deleteAliskanlik.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String aliskanlikId = mAliskanlikList.get(getAdapterPosition()).getAliskanlikId();
+                    mDatabase.child(aliskanlikId).removeValue();
+                }
+            });/*/
         }
 
         public void setData(ModelAliskanlik selectedProduct, int position) {
